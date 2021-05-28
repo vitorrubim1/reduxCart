f327c3eb78e4e4d0e323746c58b74bf4ce061f12
Vitor Rubim <Vitorrubim1@hotmail.com>
2021-05-28 15:17:57 -0300

    feat: adicionando immer para ter um codigo menos verboso

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from "redux"; //esse Reducer é preciso quando for typar o módulo
+import produce from "immer";
 
 import { ICartState } from "./types";
 
@@ -14,27 +15,26 @@ const INITIAL_STATE: ICartState = {
 
 // hero function pra ser typada, e já definindo o state
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case "ADD_PRODUCT_TO_CART": {
-      const { product } = action.payload;
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case "ADD_PRODUCT_TO_CART":
+        {
+          const { product } = action.payload;
 
-      //imutabilidade, retorno oq já tenho e adiciono ao final oq recebo
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
-            product,
-            quantity: 1,
-          },
-        ],
-      };
-    }
+          //imutabilidade, retorno oq já tenho e adiciono ao final oq recebo
 
-    default: {
-      return state;
+          //agora o draft tem o mesmo formato do state, porém consigo adicionar um item como se não houvesse imutabilidade dando push
+
+          draft.items.push({ product, quantity: 1 });
+        }
+
+        break;
+
+      default: {
+        return draft;
+      }
     }
-  }
+  }); //produce: fará a produção de um novo estado a partir do rascunho(draft) do estado anterior
 };
 
 export default cart;
