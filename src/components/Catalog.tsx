adb1f226dfd2d927d645c03b77b3356996b4c1d0
Vitor Rubim <Vitorrubim1@hotmail.com>
2021-05-29 18:24:34 -0300

    feat: abstraindo item do catalogo

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,15 +1,12 @@
-import { useCallback, useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 
-import { addProductToCart } from "../store/modules/cart/actions";
 import api from "../services/api";
 
 import { IProduct } from "../store/modules/cart/types";
+import CatalogItem from "./CatalogItem";
 
 const Catalog: React.FC = () => {
   const [catalogs, setCatalogs] = useState<IProduct[]>([]);
-
-  const dispatch = useDispatch();
 
   useEffect(() => {
     api.get("products").then((response) => {
@@ -17,25 +14,12 @@ const Catalog: React.FC = () => {
     });
   }, []);
 
-  const handleAddProductToCart = useCallback(
-    (product: IProduct) => {
-      dispatch(addProductToCart(product));
-    },
-    [dispatch]
-  );
-
   return (
     <main>
       <h1>Catalogs</h1>
 
       {catalogs.map((product) => (
-        <article key={product.id}>
-          <strong>{product.title}</strong> {" - "}
-          <span>{product.price}R$</span> {"  "}
-          <button type="button" onClick={() => handleAddProductToCart(product)}>
-            Comprar
-          </button>
-        </article>
+        <CatalogItem key={product.id} product={product} />
       ))}
     </main>
   );
